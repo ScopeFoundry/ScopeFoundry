1c76638275db599c82833aa2db657e1ad82373e9
Edward Barnard <esbarnard@lbl.gov>
2016-06-23 12:14:54 -0700

    h5_io: move h5 to new style of LQCollections

--- h5_io.py
+++ h5_io.py
@@ -48,38 +48,38 @@ other thoughts:
 
 """
 
-def h5_base_file(gui, fname):
+def h5_base_file(app, fname):
     h5_file = h5py.File(fname)
     root = h5_file['/']
-    root.attrs["ScopeFoundry_version"] = 100
+    root.attrs["ScopeFoundry_version"] = 101
     t0 = time.time()
     root.attrs['time_id'] = t0
 
-    h5_save_gui_lq(gui, root)
-    h5_save_hardware_lq(gui, root)
+    h5_save_app_lq(app, root)
+    h5_save_hardware_lq(app, root)
     return h5_file
 
-def h5_save_gui_lq(gui, h5group):
-    h5_gui_group = h5group.create_group('gui/')
-    h5_gui_group.attrs['ScopeFoundry_type'] = "Gui"
-    settings_group = h5_gui_group.create_group('settings')
-    h5_save_lqs_to_attrs(gui.logged_quantities, settings_group)
+def h5_save_app_lq(app, h5group):
+    h5_app_group = h5group.create_group('app/')
+    h5_app_group.attrs['name'] = app.name
+    h5_app_group.attrs['ScopeFoundry_type'] = "App"
+    settings_group = h5_app_group.create_group('settings')
+    h5_save_lqcoll_to_attrs(app.settings, settings_group)
 
-
-def h5_save_hardware_lq(gui, h5group):
+def h5_save_hardware_lq(app, h5group):
     h5_hardware_group = h5group.create_group('hardware/')
     h5_hardware_group.attrs['ScopeFoundry_type'] = "HardwareList"
-    for hc_name, hc in gui.hardware_components.items():
+    for hc_name, hc in app.hardware.items():
         h5_hc_group = h5_hardware_group.create_group(hc_name)
         h5_hc_group.attrs['name'] = hc.name
         h5_hc_group.attrs['ScopeFoundry_type'] = "Hardware"
         h5_hc_settings_group = h5_hc_group.create_group("settings")
-        h5_save_lqs_to_attrs(hc.logged_quantities, h5_hc_settings_group)
+        h5_save_lqcoll_to_attrs(hc.settings, h5_hc_settings_group)
     return h5_hardware_group
 
-def h5_save_lqs_to_attrs(logged_quantities, h5group):
+def h5_save_lqcoll_to_attrs(settings, h5group):
     """
-    Take a dictionary of logged_quantities
+    take a LQCollection
     and create attributes inside h5group
 
     :param logged_quantities:
@@ -88,7 +88,7 @@ def h5_save_lqs_to_attrs(logged_quantities, h5group):
     """
     unit_group = h5group.create_group('units')
     # TODO decide if we should specify h5 attr data type based on LQ dtype
-    for lqname, lq in logged_quantities.items():
+    for lqname, lq in settings.as_dict().items():
         h5group.attrs[lqname] = lq.val
         if lq.unit:
             unit_group.attrs[lqname] = lq.unit
@@ -103,7 +103,7 @@ def h5_save_measurement_settings(measurement, h5_meas_group):
     h5_meas_group.attrs['name'] = measurement.name
     h5_meas_group.attrs['ScopeFoundry_type'] = "Measurement"
     settings_group = h5_meas_group.create_group("settings")
-    h5_save_lqs_to_attrs(measurement.logged_quantities, settings_group)
+    h5_save_lqcoll_to_attrs(measurement.settings, settings_group)
     
     
 def h5_create_emd_dataset(name, h5parent, shape=None, data = None, maxshape = None, 
