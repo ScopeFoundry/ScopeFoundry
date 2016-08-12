94a834483381ecfbf613f614691712f4e11bb021
Edward Barnard <esbarnard@lbl.gov>
2016-08-12 12:12:03 -0700

    BaseCartesianScan: continuous_scan option

--- scanning/base_cartesian_scan.py
+++ scanning/base_cartesian_scan.py
@@ -76,7 +76,8 @@ class BaseCartesian2DScan(Measurement):
                                                   choices=('raster', 'serpentine', 'trace_retrace', 
                                                            'ortho_raster', 'ortho_trace_retrace'))
         
-        self.settings.New('save_h5', dtype=bool, initial=True, ro=True)
+        self.continuous_scan = self.settings.New("continuous_scan", dtype=bool, initial=False)
+        self.settings.New('save_h5', dtype=bool, initial=True, ro=False)
         
         self.settings.New('show_previous_scans', dtype=bool, initial=True)
         
@@ -472,75 +473,79 @@ class BaseCartesian2DSlowScan(BaseCartesian2DScan):
         
         self.display_image_map = np.zeros(self.scan_shape, dtype=float)
 
-        try:
-            # h5 data file setup
-            self.t0 = time.time()
-            if self.settings['save_h5']:
-                self.h5_file = h5_io.h5_base_file(self.app, "%i_%s.h5" % (self.t0, self.name) )
-                self.h5_file.attrs['time_id'] = self.t0
-                H = self.h5_meas_group  =  h5_io.h5_create_measurement_group(self, self.h5_file)
-            
-                #create h5 data arrays
-                H['h_array'] = self.h_array
-                H['v_array'] = self.v_array
-                H['range_extent'] = self.range_extent
-                H['corners'] = self.corners
-                H['imshow_extent'] = self.imshow_extent
-                H['scan_h_positions'] = self.scan_h_positions
-                H['scan_v_positions'] = self.scan_v_positions
-                H['scan_slow_move'] = self.scan_slow_move
-                H['scan_index_array'] = self.scan_index_array
-            
-            self.pre_scan_setup()
-            
-            # start scan
-            self.pixel_i = 0
-            
-            self.pixel_time = np.zeros(self.scan_shape, dtype=float)
-            if self.settings['save_h5']:
-                self.pixel_time_h5 = H.create_dataset(name='pixel_time', shape=self.scan_shape, dtype=float)            
-            
-            self.move_position_start(self.scan_h_positions[0], self.scan_v_positions[0])
-            
-            for self.pixel_i in range(self.Npixels):                
-                if self.interrupt_measurement_called: break
-                
-                i = self.pixel_i
-                
-                self.current_scan_index = self.scan_index_array[i]
-                kk, jj, ii = self.current_scan_index
-                
-                h,v = self.scan_h_positions[i], self.scan_v_positions[i]
-                
-                if self.pixel_i == 0:
-                    dh = 0
-                    dv = 0
-                else:
-                    dh = self.scan_h_positions[i] - self.scan_h_positions[i-1] 
-                    dv = self.scan_v_positions[i] - self.scan_v_positions[i-1] 
-                
-                if self.scan_slow_move[i]:
-                    self.move_position_slow(h,v, dh, dv)
-                    if self.settings['save_h5']:    
-                        self.h5_file.flush() # flush data to file every slow move
-                    #self.app.qtapp.ProcessEvents()
-                    time.sleep(0.01)
-                else:
-                    self.move_position_fast(h,v, dh, dv)
-                
-                # each pixel:
-                # acquire signal and save to data array
-                pixel_t0 = time.time()
-                self.pixel_time[kk, jj, ii] = pixel_t0
+
+        while not self.interrupt_measurement_called:        
+            try:
+                # h5 data file setup
+                self.t0 = time.time()
                 if self.settings['save_h5']:
-                    self.pixel_time_h5[kk, jj, ii] = pixel_t0
-                self.collect_pixel(self.pixel_i, kk, jj, ii)
-                S['progress'] = 100.0*self.pixel_i / (self.Npixels)
-        finally:
-            self.post_scan_cleanup()
-            if self.settings['save_h5'] and hasattr(self, 'h5_file'):
-                self.h5_file.close()
-    
+                    self.h5_file = h5_io.h5_base_file(self.app, "%i_%s.h5" % (self.t0, self.name) )
+                    self.h5_file.attrs['time_id'] = self.t0
+                    H = self.h5_meas_group  =  h5_io.h5_create_measurement_group(self, self.h5_file)
+                
+                    #create h5 data arrays
+                    H['h_array'] = self.h_array
+                    H['v_array'] = self.v_array
+                    H['range_extent'] = self.range_extent
+                    H['corners'] = self.corners
+                    H['imshow_extent'] = self.imshow_extent
+                    H['scan_h_positions'] = self.scan_h_positions
+                    H['scan_v_positions'] = self.scan_v_positions
+                    H['scan_slow_move'] = self.scan_slow_move
+                    H['scan_index_array'] = self.scan_index_array
+                
+                self.pre_scan_setup()
+                
+                # start scan
+                self.pixel_i = 0
+                
+                self.pixel_time = np.zeros(self.scan_shape, dtype=float)
+                if self.settings['save_h5']:
+                    self.pixel_time_h5 = H.create_dataset(name='pixel_time', shape=self.scan_shape, dtype=float)            
+                
+                self.move_position_start(self.scan_h_positions[0], self.scan_v_positions[0])
+                
+                for self.pixel_i in range(self.Npixels):                
+                    if self.interrupt_measurement_called: break
+                    
+                    i = self.pixel_i
+                    
+                    self.current_scan_index = self.scan_index_array[i]
+                    kk, jj, ii = self.current_scan_index
+                    
+                    h,v = self.scan_h_positions[i], self.scan_v_positions[i]
+                    
+                    if self.pixel_i == 0:
+                        dh = 0
+                        dv = 0
+                    else:
+                        dh = self.scan_h_positions[i] - self.scan_h_positions[i-1] 
+                        dv = self.scan_v_positions[i] - self.scan_v_positions[i-1] 
+                    
+                    if self.scan_slow_move[i]:
+                        self.move_position_slow(h,v, dh, dv)
+                        if self.settings['save_h5']:    
+                            self.h5_file.flush() # flush data to file every slow move
+                        #self.app.qtapp.ProcessEvents()
+                        time.sleep(0.01)
+                    else:
+                        self.move_position_fast(h,v, dh, dv)
+                    
+                    # each pixel:
+                    # acquire signal and save to data array
+                    pixel_t0 = time.time()
+                    self.pixel_time[kk, jj, ii] = pixel_t0
+                    if self.settings['save_h5']:
+                        self.pixel_time_h5[kk, jj, ii] = pixel_t0
+                    self.collect_pixel(self.pixel_i, kk, jj, ii)
+                    S['progress'] = 100.0*self.pixel_i / (self.Npixels)
+            finally:
+                self.post_scan_cleanup()
+                if self.settings['save_h5'] and hasattr(self, 'h5_file'):
+                    self.h5_file.close()
+                if not self.continuous_scan.val:
+                    break
+                
     def move_position_start(self, x,y):
         self.stage.x_position.update_value(x)
         self.stage.y_position.update_value(y)
