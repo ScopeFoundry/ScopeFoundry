0a3b70f4b422f2ed747c8dbe4653c9188e4d2cc0
Edward Barnard <esbarnard@lbl.gov>
2016-12-14 11:14:28 -0800

    DataBrowser initial version

---
+++ data_browser.py
@@ -0,0 +1,199 @@
+from __future__ import division, print_function
+from ScopeFoundry import BaseApp
+from ScopeFoundry.helper_funcs import load_qt_ui_file, sibling_path
+from collections import OrderedDict
+import os
+from PySide import QtCore, QtGui
+
+class DataBrowser(BaseApp):
+    
+    name = "DataBrowser"
+    
+    def setup(self):
+
+        self.ui = load_qt_ui_file(sibling_path(__file__, "data_browser.ui"))
+        self.ui.show()
+        self.ui.raise_()
+        
+        
+        self.views = OrderedDict()        
+
+        
+        self.settings.New('data_filename', dtype='file')
+        self.settings.New('view_name', dtype=str, initial='0', choices=('0',))
+        self.settings.view_name.updated_value.connect(self.on_change_view_name)
+        self.settings.data_filename.updated_value.connect(self.on_change_data_filename)
+        
+        self.settings.New('auto_select_view',dtype=bool, initial=True)
+        
+        # UI Connections
+        self.settings.data_filename.connect_to_browse_widgets(self.ui.data_filename_lineEdit, 
+                                                              self.ui.data_filename_browse_pushButton)
+        self.settings.view_name.connect_bidir_to_widget(self.ui.view_name_comboBox)
+        
+        # file system tree
+        self.fs_model = QtGui.QFileSystemModel()
+        #self.fs_model.setRootPath(QtCore.QDir.currentPath()) 
+        self.fs_model.setRootPath(QtCore.QDir.currentPath())
+        self.ui.treeView.setModel(self.fs_model)
+        self.ui.treeView.setRootIndex(self.fs_model.index("/Users/esbarnard/"))
+        self.ui.treeView.setIconSize(QtCore.QSize(16,16))
+        for i in (1,2,3):
+            self.ui.treeView.hideColumn(i)
+        #print("="*80, self.ui.treeView.selectionModel())
+        self.tree_selectionModel = self.ui.treeView.selectionModel()
+        self.tree_selectionModel.selectionChanged.connect(self.on_treeview_selection_change)
+
+        # set views
+        self.current_view = None
+        
+        self.load_view(FileInfoView(self))
+        self.load_view(TestNPZView(self))
+
+        self.settings['view_name'] = "file_info"
+        
+        self.console_widget.show()
+        self.ui.show()
+
+        
+        
+    def load_view(self, new_view):
+        
+        #instantiate view
+        #new_view = ViewClass(self)
+        
+        # add to views dict
+        self.views[new_view.name] = new_view
+        
+        self.ui.dataview_groupBox.layout().addWidget(new_view.ui)
+        new_view.ui.hide()
+        
+        # update choices for view_name
+        self.settings.view_name.change_choice_list(self.views.keys())
+        
+
+    def on_change_data_filename(self):
+        fname = self.settings.data_filename.val 
+        if not self.settings['auto_select_view']:
+            self.current_view.on_change_data_filename(fname)
+        else:
+            view_name = self.auto_select_view(fname)
+            if view_name == self.current_view.name:
+                self.current_view.on_change_data_filename(fname)
+            else:
+                # update view (automatically calls on_change_data_filename)
+                self.settings['view_name'] = view_name
+            
+    def on_change_view_name(self):
+        print('on_change_view_name')
+        previous_view = self.current_view
+        
+        self.current_view = self.views[self.settings['view_name']]
+    
+        # hide current view
+        if previous_view:
+            previous_view.ui.hide() 
+        else:
+            self.ui.dataview_placeholder.hide()
+        
+        # show new view
+        self.current_view.ui.show()
+        
+        # set datafile for new (current) view
+        self.current_view.on_change_data_filename(self.settings['data_filename'])
+
+    def on_treeview_selection_change(self, sel, desel):
+        fname = self.fs_model.filePath(self.tree_selectionModel.currentIndex())
+        self.settings['data_filename'] = fname
+#        print( 'on_treeview_selection_change' , fname, sel, desel)
+
+    def auto_select_view(self, fname):
+        "return the name of the last supported view for the given fname"
+        for view_name, view in self.views.items()[::-1]:
+            if view.is_file_supported(fname):
+                return view_name
+        # return default file_info view if no others work
+        return 'file_info'
+        
+
+class DataBrowserView(QtCore.QObject):
+    """ Abstract class for DataBrowser Views"""
+    
+    def __init__(self, databrowser):
+        self.databrowser =  databrowser
+        self.setup()
+        
+    def setup(self):
+        pass
+        # create view with no data file
+
+    def on_change_data_filename(self, fname=None):
+        pass
+        # load data file
+        
+        # update display
+        
+    def is_file_supported(self, fname):
+        # returns whether view can handle file, should return False early to avoid
+        # too much computation when selecting a file
+        return False
+        
+class FileInfoView(DataBrowserView):
+    
+    name = 'file_info'
+    
+    def setup(self):
+        self.ui = QtGui.QTextEdit("file_info")
+        
+    def on_change_data_filename(self, fname=None):
+        if fname is None:
+            fname = self.databrowser.settings['data_filename']
+
+        self.ui.setText(fname)
+        
+    def is_file_supported(self, fname):
+        return True
+
+
+class TestNPZView(DataBrowserView):
+    
+    name = 'test_npz_view'
+    
+    def setup(self):
+        
+        #self.ui = QtGui.QScrollArea()
+        #self.display_label = QtGui.QLabel("TestNPZView")
+        self.ui = self.display_textEdit = QtGui.QTextEdit()
+        
+        #self.ui.setLayout(QtGui.QVBoxLayout())
+        #self.ui.layout().addWidget(self.display_label)
+        #self.ui.setWidget(self.display_label)
+        
+    def on_change_data_filename(self, fname=None):
+        import numpy as np
+        
+        try:
+            self.dat = np.load(fname)
+            
+            self.display_txt = "File: {}\n".format(fname)
+            
+            for key,val in self.dat.items():
+                self.display_txt += "\t-->{} {}\n".format(key, val.shape)
+            
+            #self.display_label.setText(self.display_txt)
+            self.display_textEdit.setText(self.display_txt)
+        except Exception as err:
+            self.display_textEdit.setText("failed to load %s:\n%s" %(fname, err))
+            raise(err)
+        
+    def is_file_supported(self, fname):
+        return os.path.splitext(fname)[1] == ".npz"
+
+
+if __name__ == '__main__':
+    import sys
+    
+    app = DataBrowser(sys.argv)
+    
+    sys.exit(app.exec_())
+    
