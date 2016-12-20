a55846e94ce820ffc32c60b508c5ee671d1da70d
Edward Barnard <esbarnard@lbl.gov>
2016-12-20 11:20:53 -0800

    DataBrowser: HyperSpectralBaseView abstracts hyper-spectral data viewing. Commit includes trpl_npz, hyperspec_npz and picoharp_mcl_2d_slow_scan subclasses

--- data_browser.py
+++ data_browser.py
@@ -4,6 +4,9 @@ from ScopeFoundry.helper_funcs import load_qt_ui_file, sibling_path
 from collections import OrderedDict
 import os
 from PySide import QtCore, QtGui
+import pyqtgraph as pg
+import numpy as np
+
 
 class DataBrowser(BaseApp):
     
@@ -209,6 +212,118 @@ class TestNPZView(DataBrowserView):
         
     def is_file_supported(self, fname):
         return os.path.splitext(fname)[1] == ".npz"
+
+
+class HyperSpectralBaseView(DataBrowserView):
+    
+    name = 'HyperSpectralBaseView'
+    
+    def setup(self):
+        
+        self.ui = QtGui.QWidget()
+        self.ui.setLayout(QtGui.QVBoxLayout())
+        self.imview = pg.ImageView()
+        self.imview.getView().invertY(False) # lower left origin
+        self.ui.layout().addWidget(self.imview)
+        self.graph_layout = pg.GraphicsLayoutWidget()
+        self.ui.layout().addWidget(self.graph_layout)
+
+        self.spec_plot = self.graph_layout.addPlot()
+        self.rect_plotdata = self.spec_plot.plot()
+        self.point_plotdata = self.spec_plot.plot(pen=(0,9))
+        
+        
+        # Rectangle ROI
+        self.rect_roi = pg.RectROI([20, 20], [20, 20], pen=(0,9))
+        self.rect_roi.addTranslateHandle((0.5,0.5))        
+        self.imview.getView().addItem(self.rect_roi)        
+        self.rect_roi.sigRegionChanged.connect(self.on_change_rect_roi)
+        
+        # Point ROI
+        self.circ_roi = pg.CircleROI( (0,0), (2,2) , movable=True, pen=(0,9))
+        #self.circ_roi.removeHandle(self.circ_roi.getHandles()[0])
+        h = self.circ_roi.addTranslateHandle((0.5,.5))
+        h.pen = pg.mkPen('r')
+        h.update()
+        self.imview.getView().addItem(self.circ_roi)
+        self.circ_roi.removeHandle(0)
+        self.circ_roi_plotline = pg.PlotCurveItem([0], pen=(0,9))
+        self.imview.getView().addItem(self.circ_roi_plotline) 
+        self.circ_roi.sigRegionChanged.connect(self.on_update_circ_roi)
+        
+        self.hyperspec_data = None
+        self.display_image = None
+        self.spec_x_array = None
+        
+        self.scan_specific_setup()
+        
+    def scan_specific_setup(self):
+        #override this!
+        pass
+        
+    def is_file_supported(self, fname):
+        # override this!
+        return False
+    
+    def on_change_data_filename(self, fname):
+        try:
+            self.load_data(fname)
+            self.databrowser.ui.statusbar.clearMessage()
+            
+        except Exception as err:
+            #self.imview.setImage(np.zeros((10,10)))
+            HyperSpectralBaseView.load_data(self, fname) # load default dummy data
+            self.databrowser.ui.statusbar.showMessage("failed to load %s:\n%s" %(fname, err))
+            raise(err)
+        finally:        
+            # pyqtgraph axes are x,y, but data is stored in (frame, y,x, time), so we need to transpose        
+            self.imview.setImage(self.display_image.T)
+            self.on_change_rect_roi()
+            self.on_update_circ_roi()
+    
+    def load_data(self, fname):
+        """
+        override to set hyperspectral dataset and the display image
+        need to define:
+            * self.hyperspec_data (shape Ny, Nx, Nspec)
+            * self.display_image (shape Ny, Nx)
+            * self.spec_x_array (shape Nspec)
+        """
+        self.hyperspec_data = np.random.rand(10,10,34)
+        self.display_image = np.random.rand(10,10)
+        self.spec_x_array = 3*np.arange(34)
+        
+    def on_change_rect_roi(self):
+        # pyqtgraph axes are x,y, but data is stored in (frame, y,x, time)
+        roi_slice, roi_tr = self.rect_roi.getArraySlice(self.hyperspec_data, self.imview.getImageItem(), axes=(1,0)) 
+        
+        #print("roi_slice", roi_slice)
+        self.rect_plotdata.setData(self.spec_x_array, self.hyperspec_data[roi_slice].mean(axis=(0,1))+1)
+        
+        
+    def on_update_circ_roi(self, roi=None):
+        if roi is None:
+            roi = self.circ_roi
+
+        roi_state = roi.saveState()
+        #print roi_state
+        #xc, y
+        x0, y0 = roi_state['pos']
+        xc = x0 + 1
+        yc = y0 + 1
+        
+        #Nframe, Ny, Nx, Nt = self.time_trace_map.shape 
+        #print 'Nframe, Ny, Nx, Nt', Nframe, Ny, Nx, Nt, 
+        Ny, Nx, Nspec = self.hyperspec_data.shape
+        
+        i = max(0, min(int(xc),  Nx-1))
+        j = max(0, min(int(yc),  Ny-1))
+        
+        #print "xc,yc,i,j", xc,yc, i,j
+        
+        self.circ_roi_plotline.setData([xc, i+0.5], [yc, j + 0.5])        
+        
+        self.point_plotdata.setData(self.spec_x_array, self.hyperspec_data[j,i,:])
 
 
 if __name__ == '__main__':
