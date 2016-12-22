e8d7bf1cb0131834e42358f50e80522b15f67ba4
Edward Barnard <esbarnard@lbl.gov>
2016-12-22 13:47:32 -0800

    conda enviroments updated. Include mac and windows specific environments

--- conda_env/setup_min_conda_env.bat
+++ conda_env/setup_min_conda_env.bat
@@ -1,4 +1,4 @@
 call deactivate
 conda remove --name scopefoundry --all -y
-conda env create --name scopefoundry -f scopefoundry_min.yml
+conda env create --name scopefoundry -f scopefoundry_min_win.yml
 pause
