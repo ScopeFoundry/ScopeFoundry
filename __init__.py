df503477089ce9b08dba4de2f97a07e335c3c6e4
Edward Barnard <esbarnard@lbl.gov>
2017-01-07 10:57:00 -0800

    move base_gui.py to base_app.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-from .base_gui import BaseMicroscopeApp, BaseApp
+from ScopeFoundry.base_app import BaseMicroscopeApp, BaseApp
 from .measurement import Measurement
 from .hardware import HardwareComponent
 from .logged_quantity import LoggedQuantity, LQRange, LQCollection
