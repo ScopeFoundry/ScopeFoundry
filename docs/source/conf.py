f626027981147ebf24465e55ce3957f06333af9e
Edward Barnard <esbarnard@lbl.gov>
2017-02-02 21:05:17 -0800

    Docs config for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['PyQt', 'qtpy', 'numpy', 'h5py']
+MOCK_MODULES = ['PyQt', 'qtpy', 'numpy', 'h5py', 'pyqtgraph']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
