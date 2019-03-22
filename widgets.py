2f401b412c642e30f15e91cc66c1b9c3b6b5a1d1
Benedikt Ursprung <bursprung@lbl.gov>
2019-03-22 16:51:34 -0700

    [ScopeFoundty/widgets] added RegionSlicer class. Adds a movable pyqtgraph.LinearRegionItem to a plot and caculates the indexes of the slice defined by the region.

--- widgets.py
+++ widgets.py
@@ -4,7 +4,10 @@ Created on Jul 20, 2018
 @author: lab
 '''
 
-from qtpy import  QtCore, QtWidgets
+from qtpy import  QtCore, QtWidgets, QtGui
+import pyqtgraph as pg
+from ScopeFoundry.logged_quantity import LQCollection
+import numpy as np
 
 class MinMaxQSlider(QtWidgets.QWidget):
     """
@@ -128,3 +131,102 @@ class MinMaxQSlider(QtWidgets.QWidget):
         val = self.transform_from_slider(val)
         text =self.name+" "+str(val)
         self.title_label.setText(text)
+        
+        
+        
+        
+class RegionSlicer(object):
+    '''
+    adds a movable pyqtgraph.LinearRegionItem to 'parent_plot' and provides
+    a numpy slice that would slice specified x_array according to the region.
+    Note: The x_array from which the indexes are calculated has to be specified.
+          at initialization or using set_x_array(x_array) method
+            
+    parent_plot            <pyqtgraph.PlotItem>
+    name                   <str>
+    x_array                array-like 
+    slicer_updated_func    gets called when region is updated
+    brush,ZValue,font      are passed to the LinearRegionItem
+    initial                [start_idx, stop_idx] of the slice
+    '''
+    
+    def __init__(self, parent_plot,  x_array=None, name='array_slicer_name',
+                 slicer_updated_func=lambda:None,  
+                 brush=QtGui.QColor(0,255,0,70), ZValue=10, font=QtGui.QFont("Times", 12), initial=[0,100]):
+        self.name = name
+        self.settings = LQCollection()
+        self.start = self.settings.New('start', int, initial=initial[0], vmin = 0)
+        self.stop = self.settings.New('stop', int, initial=initial[1], vmin = 0)
+        self.activated = self.settings.New('activated', bool, initial = False)
+        
+        self.start.add_listener(self.on_change_start_stop)
+        self.stop.add_listener(self.on_change_start_stop)
+        self.activated.add_listener(self.on_change_activated)
+        
+        self.linear_region_item = pg.LinearRegionItem(brush = brush)
+        self.linear_region_item.setZValue(ZValue)        
+        parent_plot.addItem(self.linear_region_item)
+        
+        self.linear_region_item.sigRegionChangeFinished.connect(self.on_change_region)
+        self.inf_line_label = pg.InfLineLabel(self.linear_region_item.lines[1], self.name, 
+                                               position=0.78, anchor=(0.5, 0.5))
+        self.inf_line_label.setFont(font)
+        if x_array == None:
+            x_array = np.arange(512)
+        self.set_x_array(x_array)
+        self.slicer_updated = slicer_updated_func
+    
+    @QtCore.Slot(object)
+    def set_x_array(self, x_array):
+        self.x_array = np.array(x_array)
+        self.linear_region_item.setBounds( [self.x_array.min(), self.x_array.max()] )
+        kk_max = len(self.x_array)
+        self.start.change_min_max(0, kk_max)
+        self.stop.change_min_max(0, kk_max)
+        if self.start.val > kk_max:
+            self.start.update_value(kk_max)
+        if self.stop.val > kk_max:
+            self.stop.update_value(kk_max)
+        self.on_change_start_stop()
+        
+    @property
+    def slice(self):
+        return np.s_[ self.settings['start'] : self.settings['stop'] ]
+            
+    def on_change_region(self):
+        '''
+        updates settings based on region 
+        '''
+        print(self.name, 'on_change_region')
+        mn,mx = self.linear_region_item.getRegion()
+        self.settings['start'] = np.argmin( (self.x_array - mn)**2 )
+        self.settings['stop'] = np.argmin( (self.x_array - mx)**2 )
+        self.slicer_updated()
+    
+    def on_change_start_stop(self):
+        '''
+        updates linear_region_item based on settings 
+        '''
+        rgn = [0,0]
+        rgn[0] = self.x_array[ self.settings['start'] ]
+        rgn[1] = self.x_array[ self.settings['stop'] ]
+        self.linear_region_item.setRegion(rgn)    
+        
+    def on_change_activated(self):
+        activated = self.activated.val
+        self.linear_region_item.setEnabled(activated)
+        self.linear_region_item.setAcceptHoverEvents(activated)
+        self.linear_region_item.setAcceptTouchEvents(activated)
+        if activated:
+            opacity = 1
+        else:
+            opacity = 0
+        self.linear_region_item.setOpacity(opacity)
+        print('on_change_activated', activated)
+        self.slicer_updated()
+        
+    def New_UI(self):
+        ui_widget = self.settings.New_UI()
+        ui_widget.layout().insertRow(0, QtWidgets.QLabel("<b>{}</b>".format(self.name)) )        
+        return ui_widget
+        
