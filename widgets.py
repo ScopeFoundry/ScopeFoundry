0f40278773bde92da3ddd67bef948cfbbf0162b9
Benedikt Ursprung <bursprung@lbl.gov>
2019-05-20 15:10:31 -0700

    [widgets/RegionSlicer] on new x_array bugfixed

--- widgets.py
+++ widgets.py
@@ -143,6 +143,16 @@ class RegionSlicer(QtWidgets.QWidget):
     **Signals:**
     region_changed_signal()          Emitted when region is changed or activated by user
     ===============================  =============================================================================
+    
+    
+    ===============================  =============================================================================
+    **Properties:**
+    slice:                           numpy.slice[start:stop]
+    s_:                              self.slice if activativate  s_return_on_deactivated = np.s_[:] if 
+                                     deactivated.
+    mask:                            numpy boolean array which slices the x_array. 
+                                     useful if x_array is not ordered.
+    ===============================  =============================================================================    
     '''
     
     region_changed_signal = QtCore.Signal()
@@ -193,7 +203,7 @@ class RegionSlicer(QtWidgets.QWidget):
         
         if  type(plot_item) == pg.PlotDataItem:
             self.plot_data_item = plot_item
-            self.plot_data_item.sigPlotChanged.connect(self.set_x_array_from_data_data_item)
+            self.plot_data_item.sigPlotChanged.connect(self.set_x_array_from_data_item)
             self.parent_plot_item = plot_item.parentItem()
         elif type(plot_item) == pg.PlotItem:                
             self.plot_data_item = None
@@ -206,6 +216,7 @@ class RegionSlicer(QtWidgets.QWidget):
         
         self.inf_line_label = pg.InfLineLabel(self.linear_region_item.lines[label_line],
                                               self.name, position=0.78, anchor=(0.5, 0.5))
+        #self.inf_line_label = pg.TextItem(self.name, anchor=(0.5, 0.5))
         self.inf_line_label.setFont(font)
         self.set_label('')
         
@@ -214,32 +225,30 @@ class RegionSlicer(QtWidgets.QWidget):
         self.set_x_array(x_array)
         
         self.slicer_updated = slicer_updated_func
-                
+
     
     @QtCore.Slot(object)
-    def set_x_array_from_data_data_item(self):
-        print('set_x_array_from_data_data_item')
-        self.x_array,_ = self.plot_data_item.getData()
-        self.apply_new_x_array()
+    def set_x_array_from_data_item(self):
+        x_array,_ = self.plot_data_item.getData()
+        if not np.array_equal(x_array, self.x_array):
+            self.apply_new_x_array(x_array)
+            #print('set_x_array_from_data_item: new x_array()')
+        else:
+            pass
     
     def set_x_array(self, x_array):
         '''
         use this function to update the x_array
         not required to use if type(plot_item) == pg.PlotDataItem
         '''
-        self.x_array = x_array
-        self.apply_new_x_array()
+        self.apply_new_x_array(x_array)
         
-    def apply_new_x_array(self):
-        self.linear_region_item.setBounds( [self.x_array.min(), self.x_array.max()] )
-        kk_max = len(self.x_array)
-        self.start.change_min_max(0, kk_max)
-        self.stop.change_min_max(0, kk_max)
-        if self.start.val > kk_max:
-            self.start.update_value(kk_max)
-        if self.stop.val > kk_max:
-            self.stop.update_value(kk_max)
-        self.on_change_start_stop()
+    def apply_new_x_array(self, x_array):
+        mn,mx = self.linear_region_item.getRegion()
+        self.x_array = x_array        
+        self.linear_region_item.setBounds( [self.x_array.min(), self.x_array.max()] )        
+        self.settings['start'] = np.argmin( (x_array - mn)**2 )
+        self.settings['stop'] = np.argmin( (x_array - mx)**2 )        
         
     @property
     def slice(self):
@@ -308,3 +317,13 @@ class RegionSlicer(QtWidgets.QWidget):
         label = '<h3>{}</h3>{}'.format(title, text)
         self.inf_line_label.setHtml(label)
         self.inf_line_label.setColor(color)
+        
+    def get_sliced_xar(self, ar=None):
+        if ar==None:
+            if hasattr(self, 'plot_data_item'):
+                x,y = self.plot_data_item.getData()
+                return (self.x_array[self.s_], y[self.s_])                
+            else:
+                return None
+        else:
+            return (self.x_array[self.s_], ar[self.s_])
