d7ada39d0e69849164814fced01ab52878c3a072
Edward Barnard <esbarnard@lbl.gov>
2023-08-29 15:31:41 -0700

    [DataBrowser] Refactor to seperate views from Browser class

---
+++ data_browser/__init__.py
@@ -0,0 +1,4 @@
+from .data_browser import DataBrowser, DataBrowserView, FileInfoView
+from .viewers.npz import NPZView
+from .viewers.h5_tree import H5TreeView, H5TreeSearchView
+from .viewers.hyperspec_base import HyperSpectralBaseView
