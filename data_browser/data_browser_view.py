fc14de068162f9f844df4cf7e2d1b650b76c953c
Benedikt Ursprung <benediktursprung@gmail.com>
2023-12-17 17:55:05 -0500

    [data_browser_view] added helper function to check file support. Default is_file_supported method invokes function. (max execution time measured <4ms, typically ~2ms).

--- data_browser/data_browser_view.py
+++ data_browser/data_browser_view.py
@@ -3,9 +3,10 @@ from qtpy import QtCore
 from ScopeFoundry import LQCollection
 
 
-
 class DataBrowserView(QtCore.QObject):
     """ Abstract class for DataBrowser Views"""
+    
+    name = "base_view"  # override me! (recommended to use the ScopeFoundry.Measurement.name)
 
     def __init__(self, databrowser):
         QtCore.QObject.__init__(self)
@@ -25,6 +26,28 @@ class DataBrowserView(QtCore.QObject):
         # update display
 
     def is_file_supported(self, fname):
-        # returns whether view can handle file, should return False early to avoid
-        # too much computation when selecting a file
-        return False
+        """
+        returns whether view can handle file, should return False early to avoid
+        too much computation when selecting a file.         
+        
+        Override me if this fallback does not behave as expected. In particular
+            if self.name is not the supported measurement name 
+            or if supported file is not an h5 file
+        """
+        return self.check_h5_file_support(fname, (self.name,))
+        
+    def check_h5_file_support(self, fname, supported_measurement_names):
+        """
+        helper function to check if h5 file *fname* has group 'measurement/X' 
+        where X is any element of *supported_measurement_names*
+        """
+        if not fname.endswith(".h5"):
+            return False
+        
+        if isinstance(supported_measurement_names, str):
+            supported_measurement_names = (supported_measurement_names,)
+        
+        import h5py
+        with h5py.File(fname) as file:
+            return any(name in file["measurement"] for name in supported_measurement_names)
+                
