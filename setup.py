ec29fe873b7b1cd7ddda640819b1079c13c824b9
Edward Barnard <esbarnard@lbl.gov>
2024-07-01 13:52:05 -0700

    ScopeFoundry v1.4.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'ScopeFoundry',
     
-    version = '1.4.0',
+    version = '1.4.1',
     
     description = 'a platform for laboratory equipment control and scientific data analysis',
     long_description =open('README.md', 'r').read(),
