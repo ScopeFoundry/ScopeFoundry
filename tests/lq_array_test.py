c691bc6c99d7b1f7fa6e600ee625129414a3fa83
Benedikt Ursprung <bu2139@columbia.edu>
2024-10-19 16:35:35 +0200

    [base_app] moved path settings from base_microscope_app (fixes loading boolean lq_array from .ini file)

--- tests/lq_array_test.py
+++ tests/lq_array_test.py
@@ -1,3 +1,4 @@
+from functools import partial
 import logging
 
 from qtpy import QtWidgets
@@ -7,80 +8,109 @@ from ScopeFoundry.ndarray_interactive import ArrayLQ_QTableModel
 
 logging.basicConfig(level='DEBUG')
 
+
 class LQArrayTestApp(BaseApp):
 
     name = 'LQArrayTestApp'
 
-    def __init__(self, argv):
-        BaseApp.__init__(self, argv)
+    def __init__(self, argv=[], dark_mode=False):
+        super().__init__(argv, dark_mode)
 
-        self.settings.New('test_array', dtype=float, array=True,  ro=False, fmt="%1.2f",
-                  initial=[[147, 111 , 100]])#,[208, 8 , 100],[218, 45 , 100],[345, 1500 , 100,],[517, 8 , 100],[772, 300, 100]])
-
-        self.test_array_model = ArrayLQ_QTableModel(
-            self.settings.test_array, 
-            col_names=['Center','FWHM', 'Amplitude'])
+        self.settings.New(
+            "test_array",
+            dtype=float,
+            is_array=True,
+            ro=False,
+            fmt="%1.2f",
+            initial=[[147, 111, 100]],
+        )  # ,[208, 8 , 100],[218, 45 , 100],[345, 1500 , 100,],[517, 8 , 100],[772, 300, 100]])
 
         self.ui = QtWidgets.QScrollArea()
         self.ui.setWidgetResizable(True)
-        self.ui.setLayout(QtWidgets.QVBoxLayout())
+        hlayout = QtWidgets.QHBoxLayout()
+        vlayout = QtWidgets.QVBoxLayout()
 
+        self.ui.setLayout(hlayout)
+
+        self.test_array_model = ArrayLQ_QTableModel(
+            self.settings.test_array, col_names=["Center", "FWHM", "Amplitude"]
+        )
         self.tableView1 = QtWidgets.QTableView()
         self.tableView1.setModel(self.test_array_model)
-        self.ui.layout().addWidget(self.tableView1)
+        vlayout.addWidget(self.tableView1)
 
+        # repeat above
         self.tableView2 = QtWidgets.QTableView()
         self.tableView2.setModel(self.test_array_model)
-        self.ui.layout().addWidget(self.tableView2)
+        vlayout.addWidget(self.tableView2)
 
-        self.test_array_model3 = ArrayLQ_QTableModel(self.settings.test_array )
+        # no header
+        self.test_array_model3 = ArrayLQ_QTableModel(self.settings.test_array)
         self.tableView3 = QtWidgets.QTableView()
-        self.tableView3.horizontalHeader().hide()
+        # self.tableView3.horizontalHeader().hide()
         self.tableView3.setModel(self.test_array_model3)
-        self.ui.layout().addWidget(self.tableView3)
+        vlayout.addWidget(self.tableView3)
 
-        self.settings.New('test_array4', dtype=float, array=True,  ro=False, fmt="%1.2f",
-                  initial=[1, 2 , 3])
+        self.settings.New(
+            "test_array4",
+            dtype=float,
+            is_array=True,
+            ro=False,
+            fmt="%1.2f",
+            initial=[1, 2, 3],
+        )
         self.test_array_model4 = ArrayLQ_QTableModel(self.settings.test_array4 )
         self.tableView4 = QtWidgets.QTableView()
         self.tableView4.setModel(self.test_array_model4)
-        self.ui.layout().addWidget(self.tableView4)
+        vlayout.addWidget(self.tableView4)
 
         self.test_array_model5 = ArrayLQ_QTableModel(self.settings.test_array4 , transpose=True)
         self.tableView5 = QtWidgets.QTableView()
         self.tableView5.setModel(self.test_array_model5)
-        self.ui.layout().addWidget(self.tableView5)
+        vlayout.addWidget(self.tableView5)
 
         for dtype in [str, bool, int]:
             lq_name = 'test_array_{}'.format(dtype.__name__)
-            lq = self.settings.New(lq_name, dtype=dtype, array=True,  ro=False,
-                      initial=[0,1,"20",0])
+            lq = self.settings.New(
+                lq_name, dtype=dtype, is_array=True, ro=False, initial=[0, 1, "20", 0]
+            )
 
             tableModel = ArrayLQ_QTableModel(lq, row_names= [dtype.__name__,], fmt=lq.fmt, transpose=True) 
             tableView = QtWidgets.QTableView()
             tableView.setModel(tableModel)
-            self.ui.layout().addWidget(tableView)
+            vlayout.addWidget(tableView)
 
         #  int does not round
         self.settings.New("test_int", dtype=int, initial=124)
 
+        hlayout.addWidget(new_tree((self,), ["app", ""]))
+        hlayout.addLayout(vlayout)
         self.settings_ui = self.settings.New_UI()
-        self.ui.layout().addWidget(self.settings_ui)
-
-        self.ui.layout().addWidget(new_tree((self,), ["asdf", "jkl;"]))
-
-        self.load_ini_button = QtWidgets.QPushButton("Load INI file")
-        self.ui.layout().addWidget(self.load_ini_button)
-
-        self.load_ini_button.clicked.connect(self.on_load_ini_button)
+        hlayout.addWidget(self.settings_ui)
 
         self.ui.show()
-        self.console_widget.show()
+        # self.console_widget.show()
 
-    def on_load_ini_button(self):
-        self.settings_load_ini('lq_array_test.ini')
+        self.load_ini_button = QtWidgets.QPushButton("Load INI file")
+        vlayout.addWidget(self.load_ini_button)
+        self.save_ini_button = QtWidgets.QPushButton("Save INI file")
+        vlayout.addWidget(self.save_ini_button)
+        self.load_ini_button = QtWidgets.QPushButton("Load saved INI file")
+        vlayout.addWidget(self.load_ini_button)
+
+        self.load_ini_button.clicked.connect(
+            partial(self.settings_load_ini, fname="lq_array_test.ini")
+        )
+        self.save_ini_button.clicked.connect(
+            partial(self.settings_save_ini, fname="lq_array_test_saved.ini")
+        )
+        self.load_ini_button.clicked.connect(
+            partial(self.settings_load_ini, fname="lq_array_test_saved.ini")
+        )
 
 
 if __name__ == '__main__':
-    app = LQArrayTestApp([])
+    import sys
+
+    app = LQArrayTestApp(sys.argv)
     app.exec_()
