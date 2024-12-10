40fd351b550d49bbd758370e9e55b2c3e07d2863
Benedikt Ursprung <bu2139@columbia.edu>
2024-12-10 21:55:17 +0100

    [quickbar_test] decoupled from pid_control_measure.

--- tests/quickbar_test.py
+++ tests/quickbar_test.py
@@ -1,27 +1,22 @@
 import unittest
 
+from qtpy import QtWidgets
 
 from ScopeFoundry import BaseMicroscopeApp
-from ScopeFoundry.controlling import PIDFeedbackControl
 
 
 class TestApp(BaseMicroscopeApp):
 
-    name = "nested_measure_test"
-
-    def setup(self):
-
-        self.pid_m = PIDFeedbackControl(self)
-        self.add_measurement(self.pid_m)
+    name = "quickbar_test"
 
     def setup_ui(self):
-        self.add_quickbar(self.pid_m.New_mini_UI())
+        self.add_quickbar(QtWidgets.QLabel("this is a quickbar"))
 
 
 class QuickbarTest(unittest.TestCase):
 
     def test_no_crash(self):
-        self.app = TestApp([])
+        self.app = TestApp()
 
 
 if __name__ == "__main__":
