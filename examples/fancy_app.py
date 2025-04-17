54ffc8f38091450e866dc339e92e0ebca9260b03
Benedikt Ursprung <benediktursprung@gmail.com>
2025-04-17 20:42:48 +0200

    [examples] adjusted for V2 of base scan classes. Added docs files.

--- examples/fancy_app.py
+++ examples/fancy_app.py
@@ -1,20 +1,15 @@
 import sys
 
-
 from ScopeFoundry import BaseMicroscopeApp
 from ScopeFoundry.examples.example_2d_slowscan_measure import Example2DSlowScanMeasure
 from ScopeFoundry.examples.example_3d_slowscan_measure import Example3DSlowScanMeasure
-from ScopeFoundry.examples.ScopeFoundryHW.bsinc_noiser200.bsinc_noiser200_hw import (
-    Noiser200HW,
-)
-from ScopeFoundry.examples.ScopeFoundryHW.simulon_xyzstage.simulon_xyz_stage_hw import (
-    SimulonXYZStageHW,
-)
+from ScopeFoundry.examples.ScopeFoundryHW.bsinc_noiser200 import Noiser200HW
+from ScopeFoundry.examples.ScopeFoundryHW.simulon_xyzstage import SimulonXYZStageHW
 
 
-class Example2DSlowScanApp(BaseMicroscopeApp):
+class FancyApp(BaseMicroscopeApp):
 
-    name = "Example 2D Slow Scan"
+    name = "fancy app"
 
     def setup(self):
 
@@ -54,6 +49,6 @@ class Example2DSlowScanApp(BaseMicroscopeApp):
 
 
 if __name__ == "__main__":
-    app = Example2DSlowScanApp(sys.argv)
+    app = FancyApp(sys.argv)
     app.settings_load_ini("default_settings.ini")
     sys.exit(app.exec_())
