fa74b70468be04c3c832c7d74e8e689e95974bf1
Benedikt Ursprung <benediktursprung@gmail.com>
2025-06-11 17:26:29 +0200

    [tools] templates adopted for 2.1.0 tutorials

--- tools/templates/_readout.py
+++ tools/templates/_readout.py
@@ -15,7 +15,11 @@ class $READOUT_CLASS_NAME(Measurement):
     name = "$READOUT_NAME"
 
     def setup(self):
-
+        """
+        Runs once during app initialization.
+        This is where you define your settings, 
+        and set up data structures.
+        """
         s = self.settings
         s.New("save_h5", bool, initial=True)
 
@@ -23,7 +27,10 @@ class $READOUT_CLASS_NAME(Measurement):
         self.data = {"y": [2, 5, 2], "img": [[2, 4], [1, 3]]}
 
     def setup_figure(self):
-        # measurement controlls
+        """
+        Runs once during app initialization and is responsible
+        for creating a QtWidgets.QWidget self.ui.
+        """
         cb_layout = QtWidgets.QHBoxLayout()
         cb_layout.addWidget(self.new_start_stop_button())
         cb_layout.addWidget(
@@ -60,17 +67,56 @@ class $READOUT_CLASS_NAME(Measurement):
         # self.img_item.setImage(self.data["img"])
 
     def run(self):
+        """
+        Runs when the measurement starts. Executes in a separate thread from the GUI.
+        It should not update the graphical interface directly and should focus only
+        on data acquisition.
+        """
+
         print(self.__class__, "run method not implemented")
 
         # hw = self.app.hardware["$HW_NAME"]
         # self.data['y'] = hw.dev.read_data()
 
         if self.settings["save_h5"]:
-            self.save_h5_data()
+            self.save_h5(data=self.data)
 
-    def save_h5_data(self):
-        h5_file = h5_io.h5_base_file(app=self.app, measurement=self)
-        h5_meas_group = h5_io.h5_create_measurement_group(self, h5_file)
-        for k, v in self.data.items():
-            h5_meas_group[k] = v
-        h5_file.close()
+
+    # ---------------------------------------------------------------------------
+    ## UNCOMMENT IF YOU HAVE SCOPEFOUNDRY 2.0 OR EARLIER
+    # ---------------------------------------------------------------------------
+    # def save_h5(self, data):
+    #     """
+    #     opens new a .h5 file, stores *data*, and closes the file.
+
+    #     If *dataset_metadata* is None, Measurement.new_dataset_metadata() is called which updates
+    #     Measurement.dataset_metadata
+    #     """
+    #     self.open_new_h5_file(data=data)
+    #     self.close_h5_file()
+
+    # def open_new_h5_file(self, data = None):
+    #     """
+    #     sets attributes:
+    #       - Measurement.h5_file
+    #       - Measurement.h5_meas_group
+    #     returns Measurement.h5_meas_group
+    #     """
+
+    #     # there might be a file open already
+    #     self.close_h5_file()
+
+    #     from ScopeFoundry import h5_io
+
+    #     self.h5_file = h5_io.h5_base_file(self.app, measurement=self)
+    #     self.h5_meas_group = h5_io.h5_create_measurement_group(self, self.h5_file)
+
+    #     if data:
+    #         for name, value in data.items():
+    #             self.h5_meas_group.create_dataset(name=name, data=value)
+
+    #     return self.h5_meas_group 
+
+    # def close_h5_file(self):
+    #     if hasattr(self, "h5_file") and self.h5_file.id is not None:
+    #         self.h5_file.close()
