0d9585f0a5f419866c4c25785c5ffcd7caad100d
Benedikt Ursprung <benediktursprung@gmail.com>
2025-07-09 11:55:36 +0200

    [sweeping.sweep_2D_modes] added *_swap_order

--- sweeping/sweep_2D_modes.py
+++ sweeping/sweep_2D_modes.py
@@ -1,8 +1,15 @@
-SCAN_MODES = ("co-move", "nested", "serpentine")
+SCAN_MODES = (
+    "co-move",
+    "nested",
+    "nested_swap_order",
+    "serpentine",
+    "serpentine_swap_order",
+)
 SCAN_MODES_DESCRIPTION = """
 <p><i>co-move:</i> all actuators co-move
 <p><i>nested:</i> actuators move all combinations where 1st is slowest ...
 <p><i>serpentine:</i> actuators move all combinations where 1st is slowest, 2nd is fastest and 2nd reverses direction every row.
+<p><i>*_swap_order</i> modes are the same as above, but the order of the actuators is swapped.</p>
 """
 
 
@@ -10,6 +17,11 @@ def mk_positions_gen(ar_1, ar_2, mode="nested"):
     if mode == "nested":
         for k, kv in enumerate(ar_1):
             for l, lv in enumerate(ar_2):
+                yield (kv, lv)
+
+    if mode == "nested_swap_order":
+        for l, lv in enumerate(ar_2):
+            for k, kv in enumerate(ar_1):
                 yield (kv, lv)
 
     elif mode == "co-move":
@@ -25,10 +37,21 @@ def mk_positions_gen(ar_1, ar_2, mode="nested"):
                 for l, lv in reversed(list(enumerate(ar_2))):
                     yield (kv, lv)
 
+    elif mode == "serpentine_swap_order":
+        for l, lv in enumerate(ar_2):
+            if l % 2 == 0:
+                for k, kv in enumerate(ar_1):
+                    yield (kv, lv)
+            else:
+                for k, kv in reversed(list(enumerate(ar_1))):
+                    yield (kv, lv)
+
 
 def mk_data_shape(ar_1, ar_2, mode="nested"):
     if mode == "nested":
         return len(ar_1), len(ar_2)
+    elif mode == "nested_swap_order":
+        return len(ar_2), len(ar_1)
     elif mode == "co-move":
         return 1, len(ar_2)
     elif mode == "serpentine":
@@ -39,6 +62,11 @@ def mk_indices_gen(ar_1, ar_2, mode="nested"):
     if mode == "nested":
         for k, v in enumerate(ar_1):
             for l, v in enumerate(ar_2):
+                yield k, l
+
+    elif mode == "nested_swap_order":
+        for l, lv in enumerate(ar_2):
+            for k, kv in enumerate(ar_1):
                 yield k, l
 
     elif mode == "co-move":
@@ -52,6 +80,15 @@ def mk_indices_gen(ar_1, ar_2, mode="nested"):
                     yield k, l
             else:
                 for l, v in reversed(list(enumerate(ar_2))):
+                    yield k, l
+
+    elif mode == "serpentine_swap_order":
+        for l, v in enumerate(ar_2):
+            if l % 2 == 0:
+                for k, v in enumerate(ar_1):
+                    yield k, l
+            else:
+                for k, v in reversed(list(enumerate(ar_1))):
                     yield k, l
 
 
