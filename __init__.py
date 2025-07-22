e02223c31cdc7711ff9ae4101cf97c5423935c67
Benedikt Ursprung <benediktursprung@gmail.com>
2025-07-22 16:33:27 +0200

    [__init__] fixed duplicated LQCollection import

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from .base_app import BaseMicroscopeApp, BaseApp
 from .measurement import Measurement
 from .hardware import HardwareComponent
-from .logged_quantity import LQCollection, LQRange, LQCollection, LoggedQuantity
+from .logged_quantity import LQCollection, LQRange, LoggedQuantity
 from .dynamical_widgets.generic_widget import new_widget, add_to_layout
 from .dynamical_widgets.tree_widget import new_tree_widget
 from .h5_analyze_with_ipynb import analyze_with_ipynb
