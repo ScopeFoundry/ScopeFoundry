1c0687c17d6596b71c1cb54558b4b206af6f58a0
Benedikt Ursprung <benediktursprung@gmail.com>
2025-10-01 14:15:25 +0200

    [CHANGELOG, setup] for 2.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ScopeFoundry",
-    version="2.2.1",
+    version="2.3.0",
     description="a platform for laboratory equipment control and scientific data analysis",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
