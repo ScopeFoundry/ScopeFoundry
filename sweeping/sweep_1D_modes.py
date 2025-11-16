e391e86e97f9c015b68467eaa06c21ff19ad3b2c
Benedikt Ursprung <benediktursprung@gmail.com>
2025-11-16 22:56:14 +0100

    [sweep_nd_base] add Retake option to fix individual datapoints.
    (cherry picked from commit c3f276866b60b30a001cf2232d0e70750a4dddb9)

--- sweeping/sweep_1D_modes.py
+++ sweeping/sweep_1D_modes.py
@@ -1,23 +1,23 @@
-SCAN_MODES = ("co-move", "Position List")
+SCAN_MODES = ("nested", "Position List")
 SCAN_MODES_DESCRIPTION = """
-<p><i>co-move:</i> Single actuator sweep. Somewhat a trivial case - I know :-)
+<p><i>nested:</i> Single actuator sweep. Somewhat a trivial case - I know :-)</p>
 <p><i>Position List:</i> positions are defined by this Measurement's Position List.</p>
 """
 
 
-def mk_positions_gen(ar_1, mode="co-move"):
-    if mode == "co-move" or mode == "Position List":
+def mk_positions_gen(ar_1, mode="nested"):
+    if mode == "nested" or mode == "Position List":
         for k, kv in enumerate(ar_1):
             yield (kv,)
 
 
-def mk_data_shape(ar_1, mode="co-move"):
-    if mode == "co-move" or mode == "Position List":
+def mk_data_shape(ar_1, mode="nested"):
+    if mode == "nested" or mode == "Position List":
         return (len(ar_1),)
 
 
-def mk_indices_gen(ar_1, mode="co-move"):
-    if mode == "co-move" or mode == "Position List":
+def mk_indices_gen(ar_1, mode="nested"):
+    if mode == "nested" or mode == "Position List":
         for k, v in enumerate(ar_1):
             yield k,
 
